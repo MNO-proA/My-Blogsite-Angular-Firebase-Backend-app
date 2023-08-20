4dc219b9eefa86378a12841f061518348adad624
Michael Nana Ofosu <122281966+MNO-proA@users.noreply.github.com>
2023-08-20 15:33:27 +0000

    My backend firebase for my ang-blog app

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBZI3hpcL1lK1kqEeMBNW15erMAr9JbvnM',
+    authDomain: 'ang-blog-4fb73.firebaseapp.com',
+    projectId: 'ang-blog-4fb73',
+    storageBucket: 'ang-blog-4fb73.appspot.com',
+    messagingSenderId: '1019117841080',
+    appId: '1:1019117841080:web:9435f2b4b9359286dde24d',
+  },
+};
